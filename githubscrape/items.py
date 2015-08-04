c3faee639c61b0f87c3fa8b0c944b2a6f4c866c8
Manas Hardas <manas@manashardas.com>
2015-08-04 11:19:13 -0700

    Add item definition

--- githubscrape/items.py
+++ githubscrape/items.py
@@ -9,6 +9,6 @@ import scrapy
 
 
 class GithubscrapeItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
